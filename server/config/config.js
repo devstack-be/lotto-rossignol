c9fe9c7340c6f92c5612e32879bd053024d19bd5
Devstack <natanvdb@hotmail.com>
2022-05-31 19:07:19 +0200

    Update config.js

--- server/config/config.js
+++ server/config/config.js
@@ -6,9 +6,9 @@ export const development = {
   dialect: 'mysql'
 }
 export const production = {
-  host: 'eu-cdbr-west-02.cleardb.net',
-  username: 'b50826c6f33468',
-  password: '552452bd',
-  database: 'heroku_d8e47286581e2ab',
-  dialect: 'mysql'
+  host: '',
+  username: '',
+  password: '',
+  database: '',
+  dialect: ''
 }
